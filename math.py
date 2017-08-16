e15bacfe6f9bc326eb5c4fda0f1a07abfbdf0389
Andres Root <info@andresroot.co>
2017-08-15 20:49:31 -0500

    Studying TF basics

--- math.py
+++ math.py
@@ -1,1 +1,17 @@
+# Solution is available in the other "solution.py" tab
 import tensorflow as tf
+
+# TODO: Convert the following to TensorFlow:
+x = 10
+y = 2
+z = x/y - 1
+
+# TODO: Print z from a session
+
+x = tf.constant(10)
+y = tf.constant(2)
+z = tf.subtract(tf.div(x, y), 1)
+
+with tf.Session() as sess:
+    output = sess.run(z)
+    print(output)
